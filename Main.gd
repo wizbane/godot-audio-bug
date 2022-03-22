a5bbb10ce292e80787ba6519af63ff2209937b87
Wizbane <wizbane.games@gmail.com>
2022-03-21 22:57:09 -0500

    added example with only adding to scene

--- Main.gd
+++ Main.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var sfx = preload("res://AudioStreamPlayer.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,3 +17,11 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		print("Reload ----- ", get_instance_id())
 		get_tree().reload_current_scene()
+		
+	if Input.is_action_just_pressed("ui_focus_next"):
+		print("Add 5 AudioStreamPlayers (Set to -80db, stream paused)----- ")
+		for i in range(5):
+			var new_sfx = sfx.instance()
+			add_child(new_sfx)
+			
+		
